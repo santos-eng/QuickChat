9af93992fd7c84c589e2a65bbfcae8c2664d9284
santos-eng <techatarian@gmail.com>
2026-02-07 10:52:56 +1030

    working!

--- QCClient/main.cpp
+++ QCClient/main.cpp
@@ -1,5 +1,6 @@
 #include <QCNetwork/client/TCPClient.h>
 #include <iostream>
+#include <thread>
 
 using namespace QC;
 int main(int argc, char* argv[]) {
@@ -9,7 +10,21 @@ int main(int argc, char* argv[]) {
         std::cout << message;
     };
 
-    client.run();
+    // Running input processes on seperate threads, otherwise block everything
+    std::thread t{[&client]() {client.run();}};
 
+    while (true) {
+        std::string message;
+        std::getline(std::cin, message);
+
+        if (message == "\\q") break;
+        message += "\n";
+
+        client.post(message);
+    }
+
+    // Must kill the client loop and kill the thread 
+    client.stop(); 
+    t.join();
     return 0;
 }
